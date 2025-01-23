bf7b3193cbd5e66367f2c7795c9e9e80bbd82bf4
Kosisochukwu Emmanuel <realitynuel@gmail.com>
2025-01-23 11:25:00 +0100

    day 3

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,18 +1,71 @@
 const defaultResult = 0;
 let currentResult = defaultResult; 
+let logEntries = [];
 
-function add(num1,num2) {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+
+
+function getUserNumberInput() {
+    return parseInt(usrInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(operationIdentifier,prevResult,operationNumber,newResult){
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult,
+    };
+    logEntries.push(logEntry);
+    // console.log(logEntry.operation)
+    console.log(logEntries);
+    
+}
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    // currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    writeToLog('Addition', initialResult, enteredNumber, currentResult);
 
 }
 
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber
+    createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('Subtraction', initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber
+    createAndWriteOutput('*', initialResult, enteredNumber);
+    writeToLog('Multiply', initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber
+    createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('Divided', initialResult, enteredNumber, currentResult);
+}
+
+
+
 
 addBtn.addEventListener('click', add);
-
-// currentResult = (currentResult + 10) * 3 / 2 - 1; 
-
-let calculatedDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
 
